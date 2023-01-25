dc2c72ffc428f5943d38729824af3818e43d2eb5
Dani Setien <dani.setien@gmail.com>
2023-01-25 22:07:26 +0100

    add header component with H1

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,10 @@
-import Component from "../components/Component/Component";
+import Component from "./components/Component/Component";
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 
-const a = "hahaha";
-const app = new Component(document.body, "container");
-app.render();
+const appContainer = new Component(document.body, "container");
+appContainer.render();
+
+const app = document.querySelector(".container")!;
+
+const header = new HeaderComponent(app, "header", "header");
+header.render();
