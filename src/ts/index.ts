a96dab19e081b0c0c17f3024827c8cc900f928b0
Gaston82 <cabezaartaza@hotmail.com>
2023-01-26 19:10:11 +0100

    add main content

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,9 +2,13 @@ import Component from "./components/Component/Component";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 
 const appContainer = new Component(document.body, "container");
+
 appContainer.render();
 
 const app = document.querySelector(".container")!;
 
 const header = new HeaderComponent(app, "header", "header");
 header.render();
+
+const mainContent = new Component(app, "main-content", "main");
+mainContent.render();
